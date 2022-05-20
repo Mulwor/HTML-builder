a3cb302590c3e7c84c776b3e1f1615a9f844ffce
Ali <mulwor.001@yandex.ru>
2022-05-20 09:08:27 +0300

    feat: try to solve task index.js

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,12 +1,19 @@
 const fs = require('fs');                              // Для взаимодействия с файловой системой в NodeJs
 const path = require('path');                          // Для корректного указания пути к файлу 
-const { stdout  } = require('process');                 // console.log
+const {
+  stdout
+  } = require('process');                     // console.log
             
 
-const nameFile = 'text.txt';
+const nameFile = 'text.txt';                                  
 // __dirname - абсолютный путь к директории, в которой находится наш файл
+
 const pathText = path.join(__dirname, nameFile);
-const textStream = fs.createReadStream(pathText);              // Создать новый **ReadStream** из файла **text.txt**. 
-textStream.on('data', (data) => {                               // Направить поток чтения в стандартный поток вывода.  
+
+const textStream = fs.createReadStream(pathText);              
+// Создать новый **ReadStream** из файла **text.txt**. 
+
+textStream.on('data', (data) => {                               
+  // Направить поток чтения в стандартный поток вывода.  
   stdout.write(data);
 });
